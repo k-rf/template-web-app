c333eaed41e49ec24fbe4b4f221418bf4b55228f
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-10-05 13:47:15 +0000

    :fire: Remove unnecessary code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from "@nestjs/common";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { UserModule } from "./example/features/user.module";
 import { DatabaseModule } from "./lib/database/database.module";
 
 @Module({
   imports: [DatabaseModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
