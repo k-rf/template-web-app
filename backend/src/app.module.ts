b480ac3d5c4c3cefecfdbcfb69b59c8beba0ba91
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-10-05 13:45:48 +0000

    :sparkles: Add example code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { UserModule } from "./example/features/user.module";
+import { DatabaseModule } from "./lib/database/database.module";
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
