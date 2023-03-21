8ddb2e9431ed77068cfd4049894bf019cbf78aef
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-03-21 09:04:41 +0000

    :rocket: Update Dockerfile
    - 日本語入力できるようにする
    - `sudo` をパスワードなしで使えるようにする
      - ローカル開発環境のため許容している
    - 区切り文字を "-" にする

--- database/init.sh
+++ database/init.sh
@@ -3,12 +3,12 @@
 role=${DATABASE_ROLE}
 database=${DATABASE_NAME}
 
-alias psql="psql -U postgres -d ${database}"
+alias psql="psql -U postgres -d \"${database}\""
 
-psql -c "CREATE ROLE ${role} LOGIN PASSWORD '${DATABASE_PASSWORD}' CREATEDB"
-psql -c "CREATE DATABASE ${database}"
-psql -c "GRANT ALL ON DATABASE ${database} TO ${role}"
+psql -c "CREATE ROLE \"${role}\" LOGIN PASSWORD '${DATABASE_PASSWORD}' CREATEDB"
+psql -c "CREATE DATABASE \"${database}\""
+psql -c "GRANT ALL ON DATABASE \"${database}\" TO \"${role}\""
 
 psql -c "SET timezone TO 'UTC'"
 
-psql -c 'CREATE EXTENSION "uuid-ossp"'
+psql -c "CREATE EXTENSION \"uuid-ossp\""
